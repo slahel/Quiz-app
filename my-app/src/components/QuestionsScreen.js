ff480808f7d5b4cba928a8ae3975605004eda18b
slahel <74063357+slahel@users.noreply.github.com>
2022-03-28 15:26:40 +0200

    question API

--- my-app/src/components/QuestionsScreen.js
+++ my-app/src/components/QuestionsScreen.js
@@ -1,10 +1,22 @@
 import React from "react";
 
 export default function QuestionsScreen() {
+  const [questionData, setQuestionData] = React.useState({});
+  const [answer, setAnswers] = React.useState({});
+
+  React.useEffect(() => {
+    fetch(
+      "https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple"
+    )
+      .then((response) => response.json())
+      .then((data) => setQuestionData(data.results));
+  }, []);
+  console.log(questionData);
+
   return (
     <div className="questions-screen">
-      <h3>How would one say goodbye in Spanish?</h3>
-      <p>answer are here</p>
+      <h3>{questionData[0].question}</h3>
+      <p>{questionData[0].incorrect_answers}</p>
       <hr />
     </div>
   );
