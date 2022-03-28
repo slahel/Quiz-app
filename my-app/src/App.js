a64076b202511553b95d1e65a9a3ab17fab923ad
slahel <74063357+slahel@users.noreply.github.com>
2022-03-28 12:30:18 +0200

    opening screen set up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import OpeningScreen from "./components/OpeningScreen";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <OpeningScreen />
+    </div>
+  );
 }
 
 export default App;
