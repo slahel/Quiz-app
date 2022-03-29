e5a36d1358556e1537a4b4e5d9ea18f85513d18a
slahel <74063357+slahel@users.noreply.github.com>
2022-03-29 11:30:17 +0200

    css

--- my-app/src/components/Questions.js
+++ my-app/src/components/Questions.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 export default function Questions(props) {
-  console.log(props.data);
+  //   console.log(props.data);
   return (
     <div>
-      <h3>{props.data[0].question}</h3>
-      <p>{props.data[0].incorrect_answers}</p>
+      Questions
+      {/* <h3>{props.data[0].question}</h3>
+      <p>{props.data[0].incorrect_answers}</p> */}
       <hr />
     </div>
   );
--- my-app/src/components/QuestionsScreen.js
+++ my-app/src/components/QuestionsScreen.js
@@ -11,10 +11,39 @@ export default function QuestionsScreen() {
       .then((response) => response.json())
       .then((data) => setQuestionData(data.results));
   }, []);
-  //console.log(questionData);
+  // export default function App() {
+  //     const jokeElements = jokesData.map(joke => {
+  //         return (
+  //             <Joke
+  //                 key={joke.id}
+  //                 setup={joke.setup}
+  //                 punchline={joke.punchline}
+  //             />
+  //         )
+  //     })
+  //     return (
+  //         <div>
+  //             {jokeElements}
+  //         </div>
+  //     )
+  // }
+
+  // const diceElements = dice.map((die) => (
+  //   <Die
+  //     key={die.id}
+  //     value={die.value}
+  //     isHeld={die.isHeld}
+  //     holdDice={() => holdDice(die.id)}
+  //   />
+  // ));
+  const questionsElements = questionData.map((question, index) => {
+    return <Questions data={question.questionData} key={index} />;
+  });
+
   return (
     <div className="questions-screen">
-      <Questions data={questionData} />
+      {questionsElements}
+      {/* <Questions data={questionData} /> */}
       <button>Check answers</button>
     </div>
   );
