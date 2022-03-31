c264e4885510494125980636010a8f2b666afab6
slahel <74063357+slahel@users.noreply.github.com>
2022-03-31 19:31:02 +0200

    fixed Answers css

--- my-app/src/components/QuestionElements.js
+++ my-app/src/components/QuestionElements.js
@@ -1,40 +1,38 @@
 import React from "react";
 import "./Questions.css";
 import Answers from "./Answers";
+import { nanoid } from "nanoid";
 
 //import bootstrap from "bootstrap";
 
 export default function QuestionElements(props) {
-  const [chosenAnswers, setChosenAnswers] = React.useState();
+  //const [chosenAnswers, setChosenAnswers] = React.useState();
   console.log(props.data);
 
   const wrongAnswers = props.data.incorrect_answers;
   const rightAnswer = props.data.correct_answer;
-  wrongAnswers.id = nanoid();
-  rightAnswer.id = nanoid();
-
-  function holdAnswers(id) {
-    setChosenAnswers((oldItem) =>
-      oldItem.map((item) => {
-        return item.id === id ? { ...item, isHeld: !item.isHeld } : item;
-      })
-    );
-  }
 
   var shuffle = require("shuffle-array"),
     answers = wrongAnswers.concat(rightAnswer);
   shuffle(answers);
 
-  const displayedAnswers = answers.map((item) => {
-    <Answers
-      key={item.id}
-      value={item}
-      isHeld={item.isHeld}
-      holdAnswers={() => holdAnswers(item.id)}
-    />;
-
-    // return <div key={index}>{items}</div>;
+  const displayedAnswers = answers.map((item, index) => {
+    return (
+      <div key={index} id={nanoid()} className="displayed-answers">
+        {item}
+      </div>
+    );
   });
+  //   <Answers
+  //     key={item.index}
+  //     id={nanoid()}
+  //     value={item.value}
+  //     isHeld={item.isHeld}
+  //     // holdAnswers={() => holdAnswers(item.id)}
+  //   />;
+  // });
+  // console.log(displayedAnswers.value);
+  // return <div key={index}>{items}</div>;
 
   //   const diceElements = dice.map((die) => (
   //     <Die
@@ -45,10 +43,18 @@ export default function QuestionElements(props) {
   //     />
   //   ));
 
+  // function holdAnswers(id) {
+  //   setChosenAnswers((oldItem) =>
+  //     oldItem.map((item) => {
+  //       return item.id === id ? { ...item, isHeld: !item.isHeld } : item;
+  //     })
+  //   );
+  // }
+
   return (
     <div className="container">
       <h3>{props.data.question}</h3>
-      {displayedAnswers}
+      <div className="wrapper">{displayedAnswers}</div>
       <hr />
     </div>
   );
